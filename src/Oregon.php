3a4104765352273bec17ab052e2525c81deb364c
Jose Bernal <jbernal.web.dev@gmail.com>
2022-03-05 13:00:19 -0800

    Add some Oregon data.

---
+++ src/Oregon.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace GIS\Political\Countries\US;
+
+
+class Oregon {
+
+	public function getCounties() {
+
+		return array(
+			"Baker" 		=> "Baker",
+			"Benton" 		=> "Benton",
+			"Clackamas"		=> "Clackamas",
+			"Clatsop" 		=> "Clatsop",
+			"Columbia"		=> "Columbia",
+			"Coos"			=> "Coos",
+			"Crook"			=> "Crook",
+			"Curry"			=> "Curry",
+			"Deschutes"		=> "Deschutes",
+			"Douglas"		=> "Douglas",
+			"Gillam"		=> "Gillam",
+			"Grant"			=> "Grant",
+			"Harney"		=> "Harney",
+			"Hood River"	=> "Hood River",
+			"Jackson"		=> "Jackson",
+			"Jefferson"		=> "Jefferson",
+			"Josephine"		=> "Josephine",
+			"Klamath"		=> "Klamath",
+			"Lake"			=> "Lake",
+			"Lane"			=> "Lane",
+			"Lincoln"		=> "Lincoln",
+			"Linn"			=> "Linn",
+			"Malheur"		=> "Malheur",
+			"Marion"		=> "Marion",
+			"Morrow"		=> "Morrow",
+			"Multnomah"		=> "Multnomah",
+			"Polk"			=> "Polk",
+			"Sherman"		=> "Sherman",
+			"Tillamook"		=> "Tillamook",
+			"Umatilla"		=> "Umatilla",
+			"Union"			=> "Union",
+			"Wallowa"		=> "Wallowa",
+			"Wasco"			=> "Wasco",
+			"Washington"	=> "Washington",
+			"Wheeler"		=> "Wheeler",
+			"Yamhill"		=> "Yamhill"
+		);
+	}
+
+
+
+
+	public function getAppellateCourts(){
+
+		return array(
+			"" 						 => "All Courts",
+			"Oregon Appellate Court" => "Oregon Appellate Court",
+			"Oregon Supreme Court"   => "Oregon Supreme Court"
+		);
+	}
+
+
+
+}
